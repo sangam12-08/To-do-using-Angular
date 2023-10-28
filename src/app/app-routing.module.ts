c3ff32faa8f0fde606fd05666719149f074628d1
sangam.barnwal <sangam.barnwal08@gmail.com>
2023-10-28 09:28:27 +0530

    Add existing project files prior to the push to GitHub.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+//woh jo routing krte hai bina reload kiye ho jaye
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
